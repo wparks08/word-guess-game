72acf34ff2875a6af921baa0d1ee8125e45024ee
Will Parks <wparks08@gmail.com>
2019-09-26 20:11:22 -0700

    game code implemented (may need adjustment)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,41 +6,100 @@ var game = {
     wins: 0,
     remainingGuesses: 0,
     hasStarted: false,
+    hasFinished: false,
+
     initialize: function() {
         this.word = wordBank[Math.floor(Math.random() * wordBank.length)];
         this.correctGuesses = []
         this.incorrectGuesses = [];
         this.remainingGuesses = 10;
         this.hasStarted = false;
+        this.hasFinished = false;
+
         for (let i = 0; i < this.word.length; i++) {
             this.correctGuesses.push("_"); //initialize with underscores
         }
-        
-        this.updateCorrectGuesses();
     },
+
     startGame: function() {
-        this.updateCorrectGuesses;
+        this.updateDisplay();
+        this.hideStartPrompt();
         this.hasStarted = true;
     },
+
     processGuess: function(userGuess) {
+        if (this.correctGuesses.includes(userGuess) || this.incorrectGuesses.includes(userGuess) || this.hasFinished) {
+            return;
+        }
+
         if (this.word.includes(userGuess)) {
             for (let i = 0; i < this.word.length; i++) {
                 if (this.word[i] === userGuess) {
                     this.correctGuesses[i] = userGuess
                 }
-                this.updateCorrectGuesses();
+                // this.updateCorrectGuesses();
+            }
+        } else {
+            this.incorrectGuesses.push(userGuess);
+            this.remainingGuesses--;
+        }
+        this.checkGameStatus();
+        this.updateDisplay();
+    },
+
+    checkGameStatus: function() {
+        let winner = !this.correctGuesses.includes("_");
+        let loser = this.remainingGuesses == 0;
+        if ( winner || loser ) {
+            this.hasFinished = true;
+            if (winner) {
+                this.wins++;
             }
         }
-    },
-    updateCorrectGuesses: function() {
-        let text = "";
-        for (let i = 0; i < this.correctGuesses.length; i++) {
-            text = text + this.correctGuesses[i] + " ";
+
+        if (this.hasFinished) {
+            this.hasStarted = false;
+            this.displayStartPrompt();
         }
-        document.getElementById("wordDisplay").innerHTML = text;
     },
+
+    updateDisplay: function() {
+        this.updateIncorrectGuesses();
+        this.updateCorrectGuesses();
+        this.updateRemainingGuesses();
+        this.updateWins();
+    },
+
+    updateCorrectGuesses: function() {
+        document.getElementById("wordDisplay").innerHTML = this.stringifyArray(this.correctGuesses);
+    },
+
+    updateIncorrectGuesses: function() {
+        document.getElementById("lettersGuessed").innerHTML = this.stringifyArray(this.incorrectGuesses);
+    },
+
+    updateRemainingGuesses: function() {
+        document.getElementById("remainingGuesses").innerHTML = this.remainingGuesses;
+    },
+
+    updateWins: function() {
+        document.getElementById("wins").innerHTML = this.wins;
+    },
+
     displayStartPrompt: function() {
-        document.getElementById("wordDisplay").innerHTML = "Press any key to start!";
+        document.getElementById("startPrompt").innerHTML = "Press any key to start!";
+    },
+
+    hideStartPrompt: function() {
+        document.getElementById("startPrompt").innerHTML = "";
+    },
+
+    stringifyArray: function(array) {
+        let text = "";
+        for (let i = 0; i < array.length; i++) {
+            text = text + array[i] + " ";
+        }
+        return text;
     }
 };
 
@@ -50,7 +109,7 @@ document.onkeyup = function(event) {
     if (!game.hasStarted) {
         game.initialize();
         game.startGame();
-    } else {
+    } else if ((event.keyCode >= 65 && event.keyCode <= 90)) {
         game.processGuess(event.key);
-    }  
+    }
 }
